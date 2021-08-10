dd5b97a17c8b8bb688d4ef1d18324c8db38e2a62
Frederick David <fdavid@omnicomhealthgroup.com>
2021-08-10 15:41:21 +0400

    Streamlined the function and removed unused code

--- functions/roles.py
+++ functions/roles.py
@@ -1,60 +1,15 @@
 import random
 
 
-def assign_role(role_name: str, amount: int, villagers = []):
-    wolves = []
-    witches = []
-    cupids = []
-
-    i = 1
+def assign_role(amount: int, villagers = []):
+    assigned_to_the_role = []
     the_villagers = villagers
+    i = 1
+    
     while i <= amount:
         index = random.randrange(0, len(the_villagers))
-        if role_name == 'wolf':
-            wolves.append(villagers[index])
-        elif role_name == 'witch':
-            witches.append(the_villagers[index])
-        elif role_name == 'cupid':
-            cupids.append(the_villagers[index])
+        assigned_to_the_role.append(villagers[index])
         the_villagers.pop(index)
         i += 1
-
-    if role_name == 'wolf':
-        return [wolves, the_villagers]
-    elif role_name == 'witch':
-        return [witches, the_villagers]
-    elif role_name == 'cupid':
-        return [cupids, the_villagers]
-
-def assign_wolves(number_of_wolves: int, villagers = []):
-    i = 1
-    wolves = []
-    the_villagers = villagers
-    while i <= number_of_wolves:
-        index = random.randrange(0, len(the_villagers))
-        wolves.append(villagers[index])
-        the_villagers.pop(index)
-        i += 1
-    return [wolves, the_villagers]
-
-def assign_witches(number_of_witches: int, villagers = []):
-    i = 1
-    witches = []
-    the_villagers = villagers
-    while i <= number_of_witches:
-        index = random.randrange(0, len(the_villagers))
-        witches.append(the_villagers[index])
-        the_villagers.pop(index)
-        i += 1
-    return [witches, the_villagers]
-
-def assign_cupids(number_of_cupids: int, villagers = []):
-    i = 1
-    cupids = []
-    the_villagers = villagers
-    while i <= number_of_cupids:
-        index = random.randrange(0, len(the_villagers))
-        cupids.append(the_villagers[index])
-        the_villagers.pop(index)
-        i += 1
-    return [cupids, the_villagers]
+    
+    return [assigned_to_the_role, the_villagers]
